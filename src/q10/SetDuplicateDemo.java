d4e92631e510452caf1e39c9658af29a21d6e1d9
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2013-01-22 12:24:34 +0000

    done q11

--- src/q10/SetDuplicateDemo.java
+++ src/q10/SetDuplicateDemo.java
@@ -1,9 +1,38 @@
 package q10;
 
+import java.util.*;
+
 /**
  * User: Sam Wright
  * Date: 22/01/2013
  * Time: 12:00
  */
 public class SetDuplicateDemo {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> elements = new HashSet<Integer>();
+
+        // Explicitly add only unique values to elements,
+        // and add all elements (including duplicates) to set
+        for (int i = 1; i < 200; ++i) {
+            int element = (int) (Math.abs(Math.random()) * ( i % 20 ));
+            System.out.println(element);
+            if (!elements.contains(element)) {
+                elements.add(element);
+            }
+            set.add(element);
+        }
+
+        // Assert elements and set are equal
+        assert set.equals(elements);
+
+        // Make sure you can't add null twice.
+        set.add(null);
+        set.add(null);
+        assert setOf(null).equals(set);
+    }
+
+    private static <T> Set<T> setOf(T... contents) {
+        return new HashSet<T>(Arrays.asList(contents));
+    }
 }
